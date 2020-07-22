392ac4728ec994b3dc94f6af7ce91ed2bdfbcad7
guillaumetaffin <Guillaume.Taffin@opal-rt.com>
2020-07-22 18:58:54 -0400

    simplify the base, no feature creeping

--- lib/states/workouts_state.dart
+++ lib/states/workouts_state.dart
@@ -1,16 +1,7 @@
 import 'package:workout_notebook_mobile/models/workout.dart';
 
 class WorkoutsState {
-
   List<Workout> _workouts = [];
 
   List<Workout> get workouts => _workouts;
-
-  Future<void> fetchWorkouts() async {
-    _workouts = [
-      Workout('Day 1 : Chest Day', 'uuid1'),
-      Workout('Day 2 : Leg Day', 'uuid2'),
-    ];
-  }
-
 }
