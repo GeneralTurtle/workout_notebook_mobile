c09cfaf85c0183182288d988a9af96db2faba6ce
guillaumetaffin <Guillaume.Taffin@opal-rt.com>
2020-07-22 23:17:24 -0400

    first test widget !

--- test/pages/workouts_page/workouts_page_test.dart
+++ test/pages/workouts_page/workouts_page_test.dart
@@ -1,12 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:workout_notebook_mobile/main.dart';
+import 'package:workout_notebook_mobile/pages/workouts_page/workouts_page.dart';
+import 'package:workout_notebook_mobile/provider_lib/providers.dart';
+import 'package:workout_notebook_mobile/states/workouts_state.dart';
 
 void main() {
   testWidgets('Find workouts page title', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MaterialApp(
+      home: Provider<WorkoutsState>(
+        create: (ctx) => TestState(),
+        child: WorkoutsPage(),
+      ),
+    ));
 
-    final titleFinder = find.text('WORKOUTS');
+    final testState = TestState();
+    final titleFinder = find.text(testState.pageTitle);
 
     expect(titleFinder, findsOneWidget);
   });
+}
+
+class TestState implements WorkoutsState {
+  @override
+  String get pageTitle => 'Title';
 }
