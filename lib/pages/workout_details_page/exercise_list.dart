841ab593f724b34c5dd6c9025358dbd155963604
guillaumetaffin <Guillaume.Taffin@opal-rt.com>
2020-07-26 22:47:11 -0400

    use the modal bottom sheet to update an exercise info

--- lib/pages/workout_details_page/exercise_list.dart
+++ lib/pages/workout_details_page/exercise_list.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:workout_notebook_mobile/models/exercise.dart';
 import 'package:workout_notebook_mobile/states/workout_details_state.dart';
 
 class ExerciseList extends StatelessWidget {
+  final Function(
+    BuildContext ctx,
+    WorkoutDetailsState state,
+    Exercise exercise,
+  ) showBottomSheet;
+
+  const ExerciseList({Key key, @required this.showBottomSheet})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final state = Provider.of<WorkoutDetailsState>(context);
@@ -20,12 +30,20 @@ class ExerciseList extends StatelessWidget {
   Widget _buildExerciseList(WorkoutDetailsState state) {
     return ListView.builder(
       itemCount: state.exercises.length,
-      itemBuilder: (context, index) => _buildListItem(state, index),
+      itemBuilder: (context, index) => _buildListItem(context, state, index),
     );
   }
 
-  Widget _buildListItem(WorkoutDetailsState state, int index) {
+  Widget _buildListItem(
+      BuildContext context, WorkoutDetailsState state, int index) {
     final exercise = state.exercises[index];
+    return GestureDetector(
+      onTap: () => showBottomSheet(context, state, exercise),
+      child: _exerciseCard(exercise, state),
+    );
+  }
+
+  Card _exerciseCard(Exercise exercise, WorkoutDetailsState state) {
     return Card(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
